87c236a9f431e5bb83d8b09f762b96b8a4a82303
Sayat <serat5915@gmail.com>
2025-12-20 20:03:16 +0500

    fix jar run command to include all necessary parameters

--- optimize.sh
+++ optimize.sh
@@ -27,5 +27,5 @@ cd calcite_app/
 ./gradlew build
 ./gradlew shadowJar
 ./gradlew --stop
-java -Xmx4096m -jar build/libs/calcite_app-1.0-SNAPSHOT-all.jar "../input/queries" "../${OUTPUT_DIR}"
+java -Xmx4096m -jar build/libs/calcite_app-1.0-SNAPSHOT-all.jar "../input/queries" "../${OUTPUT_DIR}" "input/qop1.db" false
 cd -
