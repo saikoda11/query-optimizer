998854eba9d70346947e1c4aabb1efff9e5f172c
Sayat <serat5915@gmail.com>
2026-01-04 21:07:39 +0500

    expand calcite config

--- calcite_app/src/main/java/edu/cmu/cs/db/calcite_app/app/CalciteConfig.java
+++ calcite_app/src/main/java/edu/cmu/cs/db/calcite_app/app/CalciteConfig.java
@@ -4,11 +4,15 @@ import org.apache.calcite.avatica.util.Casing;
 import org.apache.calcite.config.CalciteConnectionConfig;
 import org.apache.calcite.config.CalciteConnectionConfigImpl;
 import org.apache.calcite.config.CalciteConnectionProperty;
+import org.apache.calcite.sql.parser.SqlParser;
+import org.apache.calcite.sql.validate.SqlValidator;
 
 import java.util.Properties;
 
 public class CalciteConfig {
     private static CalciteConnectionConfig calciteConnectionConfig;
+    private static SqlParser.Config sqlParserConfig;
+    private static SqlValidator.Config sqlValidatorConfig;
 
     public static CalciteConnectionConfig getCalciteConnectionConfig() {
         if (calciteConnectionConfig == null) {
@@ -19,5 +23,27 @@ public class CalciteConfig {
             calciteConnectionConfig = new CalciteConnectionConfigImpl(properties);
         }
         return calciteConnectionConfig;
+    }
+
+    public static SqlParser.Config getSqlParserConfig() {
+        if (sqlParserConfig == null) {
+            sqlParserConfig = SqlParser.config()
+                    .withCaseSensitive(calciteConnectionConfig.caseSensitive())
+                    .withUnquotedCasing(calciteConnectionConfig.unquotedCasing())
+                    .withQuotedCasing(calciteConnectionConfig.quotedCasing())
+                    .withConformance(calciteConnectionConfig.conformance());
+        }
+        return sqlParserConfig;
+    }
+
+    public static SqlValidator.Config getSqlValidatorConfig() {
+        if (sqlValidatorConfig == null) {
+            sqlValidatorConfig = SqlValidator.Config.DEFAULT
+                    .withLenientOperatorLookup(calciteConnectionConfig.lenientOperatorLookup())
+                    .withConformance(calciteConnectionConfig.conformance())
+                    .withDefaultNullCollation(calciteConnectionConfig.defaultNullCollation())
+                    .withIdentifierExpansion(true);
+        }
+        return sqlValidatorConfig;
     }
 }
