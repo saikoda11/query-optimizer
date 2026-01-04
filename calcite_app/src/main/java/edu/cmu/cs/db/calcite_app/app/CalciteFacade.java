a405450a10a94aeabc9a21171ba81695e679183d
Sayat <serat5915@gmail.com>
2026-01-04 21:07:58 +0500

    refactoring calcite facade

--- calcite_app/src/main/java/edu/cmu/cs/db/calcite_app/app/CalciteFacade.java
+++ calcite_app/src/main/java/edu/cmu/cs/db/calcite_app/app/CalciteFacade.java
@@ -1,15 +1,14 @@
 package edu.cmu.cs.db.calcite_app.app;
 
-import org.apache.calcite.avatica.util.Casing;
-import org.apache.calcite.config.CalciteConnectionConfigImpl;
-import org.apache.calcite.config.CalciteConnectionProperty;
+import org.apache.calcite.config.CalciteConnectionConfig;
 import org.apache.calcite.jdbc.CalciteSchema;
 import org.apache.calcite.jdbc.JavaTypeFactoryImpl;
-import org.apache.calcite.plan.RelOptCluster;
-import org.apache.calcite.plan.RelOptTable;
+import org.apache.calcite.plan.*;
+import org.apache.calcite.plan.volcano.VolcanoPlanner;
 import org.apache.calcite.prepare.CalciteCatalogReader;
 import org.apache.calcite.prepare.Prepare;
 import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.RelRoot;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.rex.RexBuilder;
 import org.apache.calcite.sql.SqlNode;
@@ -23,69 +22,83 @@ import org.apache.calcite.sql2rel.StandardConvertletTable;
 import org.apache.calcite.util.SourceStringReader;
 
 import java.util.Collections;
-import java.util.Properties;
 
 public class CalciteFacade {
-    private final CalciteConnectionConfigImpl config;
+    private final CalciteConnectionConfig config = CalciteConfig.getCalciteConnectionConfig();
     private final SqlValidator validator;
     private final SqlToRelConverter sqlToRelConverter;
     private final Optimizer optimizer = new Optimizer();
+    private final RelDataTypeFactory typeFactory = CustomSchema.getTypeFactory();
+    private final CustomSchema customSchema;
 
-    public CalciteFacade(CalciteSchema calciteSchema) {
-        Properties configProperties = new Properties();
-        configProperties.put(CalciteConnectionProperty.CASE_SENSITIVE.camelName(), Boolean.TRUE.toString());
-        configProperties.put(CalciteConnectionProperty.UNQUOTED_CASING.camelName(), Casing.UNCHANGED.toString());
-        configProperties.put(CalciteConnectionProperty.QUOTED_CASING.camelName(), Casing.UNCHANGED.toString());
-        this.config = new CalciteConnectionConfigImpl(configProperties);
-        RelDataTypeFactory typeFactory = new JavaTypeFactoryImpl();
+    public CalciteFacade(String duckDbFilePath) {
+        customSchema = CustomSchema.create(duckDbFilePath);
+        this.validator = createValidator();
+
+        VolcanoPlanner planner = new VolcanoPlanner(
+                RelOptCostImpl.FACTORY,
+                Contexts.of(config)
+        );
+
+        planner.addRelTraitDef(ConventionTraitDef.INSTANCE);
+
+        RelOptCluster cluster = RelOptCluster.create(
+                planner,
+                new RexBuilder(typeFactory)
+        );
+
+        sqlToRelConverter = createConverter(cluster);
+    }
+
+    private SqlValidator createValidator() {
+        CalciteSchema rootSchema = CalciteSchema.createRootSchema(false, false);
+        rootSchema.add(customSchema.getSchemaName(), customSchema);
         Prepare.CatalogReader catalogReader = new CalciteCatalogReader(
-                calciteSchema,
-                Collections.singletonList("default"),
+                rootSchema,
+                Collections.singletonList(customSchema.getSchemaName()),
                 typeFactory,
                 config);
 
-        this.validator = SqlValidatorUtil.newValidator(
+        SqlValidator.Config sqlValidatorConfig = SqlValidator.Config.DEFAULT
+                .withDefaultNullCollation(config.defaultNullCollation())
+                .withConformance(config.conformance())
+                .withIdentifierExpansion(true);
+
+        return SqlValidatorUtil.newValidator(
                 SqlStdOperatorTable.instance(),
                 catalogReader,
                 typeFactory,
-                SqlValidator.Config.DEFAULT
-                        .withDefaultNullCollation(config.defaultNullCollation())
-                        .withConformance(config.conformance())
-                        .withIdentifierExpansion(true));
+                sqlValidatorConfig
+        );
+    }
 
-        RelOptCluster cluster = RelOptCluster.create(optimizer.getPlanner(), new RexBuilder(typeFactory));
-
-        RelOptTable.ViewExpander NOOP_EXPANDER = (type, query, schema, path) -> null;
-        this.sqlToRelConverter = new SqlToRelConverter(
-                NOOP_EXPANDER,
-                this.validator,
-                catalogReader,
+    private SqlToRelConverter createConverter(RelOptCluster cluster) {
+        SqlToRelConverter.Config converterConfig = SqlToRelConverter.config()
+                .withTrimUnusedFields(true)
+                .withExpand(false);
+//        RelOptTable.ViewExpander NOOP_EXPANDER = (type, query, schema, path) -> null;
+        return new SqlToRelConverter(
+                null,
+                validator,
+                customSchema.getCatalogReader(),
                 cluster,
                 StandardConvertletTable.INSTANCE,
-                SqlToRelConverter.config()
-                        .withExpand(true));
+                converterConfig
+        );
     }
 
     public SqlNode parse(String sql) throws SqlParseException {
-        SqlParser sqlParser = SqlParser.create(
-                new SourceStringReader(sql),
-                SqlParser.config()
-                        .withCaseSensitive(config.caseSensitive())
-                        .withUnquotedCasing(config.unquotedCasing())
-                        .withQuotedCasing(config.quotedCasing())
-                        .withConformance(config.conformance())
-        );
+        SqlParser.Config sqlParserConfig = CalciteConfig.getSqlParserConfig();
+        SqlParser sqlParser = SqlParser.create(sql, sqlParserConfig);
         return sqlParser.parseQuery();
     }
 
     public SqlNode validate(SqlNode sqlNode) {
-        return this.validator.validate(sqlNode);
+        return validator.validate(sqlNode);
     }
 
     public RelNode sql2rel(SqlNode sqlNode) {
-        return this.sqlToRelConverter
-                .convertQuery(
-                        sqlNode, false, true
-                ).rel;
+        RelRoot root = sqlToRelConverter.convertQuery(sqlNode, false, true);
+        return root.rel;
     }
 }
